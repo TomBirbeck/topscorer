0c1fa38c76ab2f701ed4b2da59cd9b7046fc0467
unknown <tombirbeck47@gmail.com>
2022-09-07 23:36:37 +0100

    test for models that will be used for now done, routes next

--- backend/models/players.js
+++ backend/models/players.js
@@ -1,5 +1,18 @@
+import pool from '../db/index.js'
 
 export async function getPlayers(){
     const res = await pool.query('SELECT * FROM players;');
     return res.rows;
 }
+
+export async function getTopScorerByYear(year) {
+    const res = await pool.query('SELECT * FROM $1);', [`table_${year}`]);
+    return res.rows;
+}
+
+export async function getTopScorerByLeagueAndYear(league, year) {
+    const res = await pool.query('SELECT * FROM $1 WHERE league = $2;', [year, league]);
+}
+
+//SELECT table_name FROM information_schema.tables
+// WHERE table_schema = 'your_database_name';
--- backend/models/players.test.js
+++ backend/models/players.test.js
@@ -1,7 +1,7 @@
 // import request from 'supertest';
 // import app from '../app';
 import { test, expect } from '@jest/globals';
-// import { getPlayers } from './players.js';
+import { getTopScorerByYear, getTopScorerByLeagueAndYear } from './players.js';
 
 describe('tesing player models', () => {
   //   test('when getPlayers function is called, all players are returned', () => {
@@ -37,26 +37,7 @@ describe('tesing player models', () => {
   //   });
 
   test('when getTopScorerByYear function is called, returns all players from that season', () => {
-    async function getTopScorerByYear(season) {
-      return {
-        success: true,
-        data: [
-          {
-            firstName: 'Dave',
-            surName: 'Test',
-            dob: '12-04-1990',
-            club: 'AEK Athens',
-            imgURL: 'www.img.com',
-            season: '1992/93',
-            league: 'Greek',
-            appearances: 30,
-            goals: 15,
-            GPG: 0.5,
-          },
-        ],
-      };
-    }
-    const actual = getTopScorerByYear(1992 / 93);
+    const actual = getTopScorerByYear(1992);
 
     const expected = {
       success: true,
@@ -67,7 +48,7 @@ describe('tesing player models', () => {
           dob: expect.any(String),
           club: expect.any(String),
           imgURL: expect.any(String),
-          season: '1992/93',
+          season: '1992',
           league: expect.any(String),
           appearances: expect.any(Number),
           goals: expect.any(Number),
@@ -78,4 +59,30 @@ describe('tesing player models', () => {
 
     expect(actual).resolves.toStrictEqual(expected);
   });
+
+  test('when getTopScorerByLeague function is called, returns all players from that season', () => {
+    const actual = getTopScorerByLeagueAndYear(1993, 'Ligue 1');
+
+    const expected = {
+      success: true,
+      data: [
+        {
+          firstName: expect.any(String),
+          surName: expect.any(String),
+          dob: expect.any(String),
+          club: expect.any(String),
+          imgURL: expect.any(String),
+          season: 1993,
+          league: 'Ligue 1',
+          appearances: expect.any(Number),
+          goals: expect.any(Number),
+          GPG: expect.any(Number),
+        },
+      ],
+    };
+
+    expect(actual).resolves.toStrictEqual(expected);
+  });
+
+
 });

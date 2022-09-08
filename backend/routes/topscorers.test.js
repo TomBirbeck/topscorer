c649e8dbfd86ccd9c269f8067f4c5660d84e8ccd
unknown <tombirbeck47@gmail.com>
2022-09-08 10:23:13 +0100

    working on topscorer route testing

--- backend/routes/topscorers.test.js
+++ backend/routes/topscorers.test.js
@@ -1,3 +1,14 @@
 import request from 'supertest';
 import app from '../app';
 import { test, expect } from '@jest/globals';
+
+describe('route tests', () => {
+    test('app working', () => {
+        app.get('/topscorer', (req, res) => {
+            res.status(200)
+        })
+    })
+
+
+
+})

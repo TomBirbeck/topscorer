0b9c805df03c0a9e611ab6313ce4ae51f96f9fd6
unknown <tombirbeck47@gmail.com>
2022-09-15 17:34:20 +0100

    added image links up to 1998

---
+++ backend/db/rooney.js
@@ -0,0 +1,68 @@
+
+        
+            
+                {
+                    firstname: 'Wayne',
+                    surname: 'Rooney',
+                    dob: '24-10-1985',
+                    nationality: 'English',
+                    club: 'Manchester United',
+                    season: 2010,
+                    league: 'epl',
+                    img: 'https://th.bing.com/th/id/OIP.Dj2ZbCPTRpEP64z3aKCqHQHaJY?w=160&h=184&c=7&r=0&o=5&pid=1.7',
+                    appearances:28,
+                    goals: 11,
+                    // gpa:
+                    },
+                    {
+                        firstname: 'Wayne',
+                        surname: 'Rooney',
+                        dob: '24-10-1985',
+                        nationality: 'English',
+                        club: 'Manchester United',
+                        season: 2011,
+                        league: 'epl',
+                        img: 'https://th.bing.com/th/id/OIP.Dj2ZbCPTRpEP64z3aKCqHQHaJY?w=160&h=184&c=7&r=0&o=5&pid=1.7',
+                        appearances:34,
+                        goals: 27,
+                        // gpa:
+                        },
+                        {
+                            firstname: 'Wayne',
+                            surname: 'Rooney',
+                            dob: '24-10-1985',
+                            nationality: 'English',
+                            club: 'Manchester United',
+                            season: 2012,
+                            league: 'epl',
+                            img: 'https://th.bing.com/th/id/OIP.Dj2ZbCPTRpEP64z3aKCqHQHaJY?w=160&h=184&c=7&r=0&o=5&pid=1.7',
+                            appearances:27,
+                            goals: 12,
+                            // gpa:
+                            },
+                            {
+                                firstname: 'Wayne',
+                                surname: 'Rooney',
+                                dob: '24-10-1985',
+                                nationality: 'English',
+                                club: 'Manchester United',
+                                season: 2013,
+                                league: 'epl',
+                                img: 'https://th.bing.com/th/id/OIP.Dj2ZbCPTRpEP64z3aKCqHQHaJY?w=160&h=184&c=7&r=0&o=5&pid=1.7',
+                                appearances:29,
+                                goals: 17,
+                                // gpa:
+                                },
+                                {
+                                    firstname: 'Wayne',
+                                    surname: 'Rooney',
+                                    dob: '24-10-1985',
+                                    nationality: 'English',
+                                    club: 'Manchester United',
+                                    season: 2005,
+                                    league: 'epl',
+                                    img: 'https://th.bing.com/th/id/OIP.Dj2ZbCPTRpEP64z3aKCqHQHaJY?w=160&h=184&c=7&r=0&o=5&pid=1.7',
+                                    appearances:33,
+                                    goals: 12,
+                                    // gpa:
+                                    },

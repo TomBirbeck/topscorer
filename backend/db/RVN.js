77277072f757cfd24ee76af994ded4f1d24968c6
unknown <tombirbeck47@gmail.com>
2022-09-15 10:44:44 +0100

    epl up to 2009-10 season entered

---
+++ backend/db/RVN.js
@@ -0,0 +1,41 @@
+{
+    firstname: 'Emmanuel',
+    surname: 'Adebayor',
+    dob: '22-02-1984',
+    nationality: 'Togolgese',
+    club: 'Manchester City',
+    season: 2009,
+    league: 'epl',
+    img: '',
+    appearances:26,
+    goals: 14,
+    // gpa:
+    },
+    {
+        firstname: 'Emmanuel',
+        surname: 'Adebayor',
+        dob: '22-02-1984',
+        nationality: 'Togolgese',
+        club: 'Tottenham Hotspurs (Loan)',
+        season: 2011,
+        league: 'epl',
+        img: '',
+        appearances:33,
+        goals: 17,
+        // gpa:
+        },
+        {
+            firstname: 'Emmanuel',
+            surname: 'Adebayor',
+            dob: '22-02-1984',
+            nationality: 'Togolgese',
+            club: 'Tottenham Hotspurs',
+            season: 2013,
+            league: 'epl',
+            img: '',
+            appearances:21,
+            goals: 11,
+            // gpa:
+            },
+
+
